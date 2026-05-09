50d69e6da6ef74c6e7fae8a37fc212b841b9a3eb
S-S-Karthika <karthikagithub2004@gmail.com>
2026-05-09 23:31:31 +0530

    pool change

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000' || 'https://womens-choice-one.vercel.app',
+  origin: 'http://localhost:3000' || 'https://womens-choice-one.vercel.app',
   credentials: true,
 }));
 app.use(express.json());
